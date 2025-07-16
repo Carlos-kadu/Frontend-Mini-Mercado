ccf2d560180ea1f6c9f93810c27db67ec82b4426
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-07-16 17:14:50 -0300

    feat(app): nova rota de visualização de produto

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,32 @@ import EmpresaForm from './pages/EmpresaForm';
 import FiliaisList from './pages/FiliaisList';
 import FilialForm from './pages/FilialForm';
 import ProdutosPage from './pages/ProdutosPage';
+import ProdutoForm from './pages/ProdutoForm';
+import ProdutoView from './pages/ProdutoView';
+import { SearchProvider } from './searchContext.jsx';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <main className="container mt-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/empresas" element={<EmpresasList />} />
-          <Route path="/empresas/nova" element={<EmpresaForm />} />
-          <Route path="/empresas/editar/:id" element={<EmpresaForm />} />
-          <Route path="/filiais" element={<FiliaisList />} />
-          <Route path="/filiais/nova" element={<FilialForm />} />
-          <Route path="/filiais/editar/:id" element={<FilialForm />} />
-          <Route path="/produtos/*" element={<ProdutosPage />} />
-        </Routes>
-      </main>
-    </Router>
+    <SearchProvider>
+      <Router>
+        <Navbar />
+        <main className="container mt-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/empresas" element={<EmpresasList />} />
+            <Route path="/empresas/nova" element={<EmpresaForm />} />
+            <Route path="/empresas/editar/:id" element={<EmpresaForm />} />
+            <Route path="/filiais" element={<FiliaisList />} />
+            <Route path="/filiais/nova" element={<FilialForm />} />
+            <Route path="/filiais/editar/:id" element={<FilialForm />} />
+            <Route path="/produtos/*" element={<ProdutosPage />} />
+            <Route path="/produtos/:categoria/novo" element={<ProdutoForm />} />
+            <Route path="/produtos/:categoria/visualizar/:id" element={<ProdutoView />} />
+          </Routes>
+        </main>
+      </Router>
+    </SearchProvider>
   );
 }
 
