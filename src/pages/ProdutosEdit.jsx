691a26dbbbc646856acfb5bb1f7e991ca6b046db
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-07-17 13:49:13 -0300

    feat(produto): componente para edição de produto

---
+++ src/pages/ProdutosEdit.jsx
@@ -0,0 +1,10 @@
+import ProdutoForm from './ProdutoForm';
+import React from 'react';
+
+export default function ProdutosEdit() {
+  return (
+    <div className="container mt-4">
+      <ProdutoForm />
+    </div>
+  );
+} 
