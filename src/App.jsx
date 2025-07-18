ca0f622623216dc0af5d648e87b936263e73688c
Carlos Eduardo Rodrigues <eduardokadu831@gmail.com>
2025-07-18 10:27:55 -0300

    refactor(app): ajustes gerais no componente principal

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,26 @@ import FilialForm from './pages/FilialForm';
 import ProdutosPage from './pages/ProdutosPage';
 import ProdutoForm from './pages/ProdutoForm';
 import ProdutoView from './pages/ProdutoView';
-import { SearchProvider } from './searchContext.jsx';
 import './App.css';
 
 function App() {
   return (
-    <SearchProvider>
-      <Router>
-        <Navbar />
-        <main className="container mt-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/empresas" element={<EmpresasList />} />
-            <Route path="/empresas/nova" element={<EmpresaForm />} />
-            <Route path="/empresas/editar/:id" element={<EmpresaForm />} />
-            <Route path="/filiais" element={<FiliaisList />} />
-            <Route path="/filiais/nova" element={<FilialForm />} />
-            <Route path="/filiais/editar/:id" element={<FilialForm />} />
-            <Route path="/produtos/*" element={<ProdutosPage />} />
-            <Route path="/produtos/:categoria/novo" element={<ProdutoForm />} />
-            <Route path="/produtos/:categoria/visualizar/:id" element={<ProdutoView />} />
-          </Routes>
-        </main>
-      </Router>
-    </SearchProvider>
+    <Router>
+      <Navbar />
+      <main className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/empresas" element={<EmpresasList />} />
+          <Route path="/empresas/nova" element={<EmpresaForm />} />
+          <Route path="/empresas/editar/:id" element={<EmpresaForm />} />
+          <Route path="/filiais" element={<FiliaisList />} />
+          <Route path="/filiais/nova" element={<FilialForm />} />
+          <Route path="/filiais/editar/:id" element={<FilialForm />} />
+          <Route path="/produtos/*" element={<ProdutosPage />} />
+          <Route path="/produtos/:categoria/novo" element={<ProdutoForm />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
